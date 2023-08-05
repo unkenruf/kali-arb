16847085103293d67c2746b10e72c539244f0cb3
unkenruf <unkenruf@gmail.com>
2023-08-05 01:58:51 -0400

    updated main script to adjust for sudoer tasks

--- arb.sh
+++ arb.sh
@@ -1,34 +1,46 @@
 #!/bin/bash
 
+if ! [ $(id -u) = 0 ]; then
+	echo "The script needs to be run as root." >&2
+	exit 1
+fi
+
+if [ $SUDO_USER ]; then
+	real_user=$SUDO_USER
+else
+	real_user=$(whoami)
+fi
+
 # Initial Setup
 apt install mugshot xfce4-terminal -y
 update-alternatives --set x-terminal-emulator /usr/bin/xfce4-terminal.wrapper
 
 # Install themes, icons, cursors, and fonts
-mkdir $HOME/.themes
-mkdir $HOME/.local/share/icons
-mv GTK-XFWM-Theme/Ever* $HOME/.themes
-unzip Nordzy-cyan-dark-MOD.zip -d $HOME/.local/share/icons
-git clone https://github.com/alvatip/Radioactive-nord.git
-cd Radioactive-nord
-sudo ./install.sh
+sudo -u $real_user mkdir $HOME/.themes
+sudo -u $real_user mkdir $HOME/.local/share/icons
+sudo -u $real_user mv GTK-XFWM-Theme/Ever* $HOME/.themes
+sudo -u $real_user unzip Nordzy-cyan-dark-MOD.zip 
+sudo -u $real_user cp -r Nordzy-cyan-dark-MOD $HOME/.local/share/icons
+sudo -u $real_user git clone https://github.com/alvatip/Radioactive-nord.git
+sudo -u $real_user cd Radioactive-nord
+./install.sh
 cd ..
-unzip fonts.zip -d $HOME/.local/share/
+sudo -u $real_user unzip fonts.zip -d $HOME/.local/share/
 
 # Installing Kvantum theme and fonts
-sudo apt install qt5-style-kvantum qt5-style-kvantum-themes -y
-cp -r Kvantum $HOME/.config/
+apt install qt5-style-kvantum qt5-style-kvantum-themes -y
+sudo -u $real_user cp -r Kvantum $HOME/.config/
 
 # Config LightDM Login Manager
-sudo cp -r $HOME/.themes/GTK-XFWM-Everblush-Theme/Everblush /usr/share/themes
-sudo cp -Rv $HOME/.local/share/icons/Nordzy-cyan-dark-MOD /usr/share/icons
-sudo mv  lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
+cp -r GTK-XFWM-Everblush-Theme/Everblush /usr/share/themes
+cp -Rv Nordzy-cyan-dark-MOD /usr/share/icons
+cp lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
 
 # Installing and Config Picom
-sudo apt install libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libpcre3-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev meson -y
-sudo apt install picom -y
-cp picom-config/picom.conf $HOME/.config
-sudo xfconf-query -c xfwm4 -p /general/use_compositing -t bool -s false
+apt install libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libpcre3-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev meson -y
+apt install picom -y
+sudo -u $real_user picom-config/picom.conf $HOME/.config
+xfconf-query -c xfwm4 -p /general/use_compositing -t bool -s false
 
 # Configure xfce4-panel and dock-like plugin
 ## Panel
@@ -43,26 +55,26 @@ cp -r xfce4 $HOME/.config/
 
 
 ## Dock-like Plugin
-sudo apt install xfce4-dev-tools libstartup-notification0-dev libwnck-3-dev libxfce4ui-2-dev libxfce4panel-2.0-dev -y
+apt install xfce4-dev-tools libstartup-notification0-dev libwnck-3-dev libxfce4ui-2-dev libxfce4panel-2.0-dev -y
 git clone https://gitlab.xfce.org/panel-plugins/xfce4-docklike-plugin.git
 cd xfce4-docklike-plugin
-sudo ./autogen.sh --prefix=/usr/local
-sudo make
-sudo make install
-sudo cp src/docklike.desktop /usr/share/xfce4/panel/plugins
-sudo cp src/.libs/libdocklike.so /usr/lib/x86_64-linux-gnu/xfce4/panel/plugins/
-sudo cp src/libdocklike.la /usr/lib/x86_64-linux-gnu/xfce4/panel/plugins
+./autogen.sh --prefix=/usr/local
+make
+make install
+cp src/docklike.desktop /usr/share/xfce4/panel/plugins
+cp src/.libs/libdocklike.so /usr/lib/x86_64-linux-gnu/xfce4/panel/plugins/
+cp src/libdocklike.la /usr/lib/x86_64-linux-gnu/xfce4/panel/plugins
 
 # Installing and Configuring Findex
-sudo apt install libkeybinder-3.0-dev cargo -y
+apt install libkeybinder-3.0-dev cargo -y
 git clone https://github.com/mdgaziur/findex.git
 cd findex
-sudo ./installer.sh
+./installer.sh
 
 # Installing Lock Screen
-sudo apt install i3lock-color -y
-sudo xfconf-query --create -c xfce4 session -p /general/LockCommand -t string -s "i3lock-everblush"
-sudo cp i3lock-color-everblush/i3lock-everblush /usr/bin
+apt install i3lock-color -y
+xfconf-query --create -c xfce4 session -p /general/LockCommand -t string -s "i3lock-everblush"
+cp i3lock-color-everblush/i3lock-everblush /usr/bin
 
 echo "REBOOT me please" 
 
